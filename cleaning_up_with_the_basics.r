8ba67732d751aac15bea68f4864b1cfb5a8a4c1d
Olesia Dolzhenkova <olesinionz@gmail.com>
2024-01-16 20:02:09 +1300

    practice more with renaming

--- cleaning_up_with_the_basics.r
+++ cleaning_up_with_the_basics.r
@@ -8,3 +8,27 @@ install.packages("dplyr")
 library("dplyr")
 install.packages("palmerpenguins")
 library("palmerpenguins")
+skim_without_charts(penguins)
+glimpse(penguins)
+head(penguins)
+
+#Just species colmn
+penguins %>% 
+  select(species)
+
+#Exept species colmn
+penguins %>% 
+  select(-species)
+
+penguins %>% 
+  rename(island_new = island)
+
+#Make all colum names are uppercase
+rename_with(penguins, toupper)
+
+#Make all colum names are lowercase
+rename_with(penguins, tolower)
+
+#Make sure it's only numbers, characters and underscoe in the names
+clean_names(penguins)
+
