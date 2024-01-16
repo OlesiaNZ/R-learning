7e88942767ee606789df40fc72e250fb449168bf
Olesia Dolzhenkova <olesinionz@gmail.com>
2024-01-16 22:13:53 +1300

    do separate and unite functions

--- manual_create_data_frame.r
+++ manual_create_data_frame.r
@@ -6,3 +6,22 @@ job_title <- c("Professional", "Programmer", "Management", "Clerical", "Develope
 
 employee <- data.frame(id, name, job_title)
 View(employee)
+print(employee)
+
+separate(employee, name, into = c('first_name', 'last_name'))
+separate(employee, name, into = c('first_name', 'last_name'), sep = ' ')
+
+#employee2 <- employee(id, first_name, last_name, job_title)
+#print(employee2)
+
+first_name <- c("John", "Rob", "Rachel", "Christy", "Johnson", "Candace", "Carlson", "Pansy", "Darius", "Claudia")
+
+last_name <- c("Mendes", "Stewart", "Abrahamson", "Hickman", "Harper", "Miller", "Landy", "Jordan", "Berry", "Garcia")
+
+job_title <- c("Professional", "Programmer", "Management", "Clerical", "Developer", "Programmer", "Management", "Clerical", "Developer", "Programmer")
+
+employee <- data.frame(id, first_name, last_name, job_title)
+
+print(employee)
+
+unite(employee, 'name', first_name, last_name, sep = ' ')
