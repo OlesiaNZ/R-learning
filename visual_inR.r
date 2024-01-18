906802a0fb0f90163f9e3cec6191edcaafcd2493
Olesia Dolzhenkova <olesinionz@gmail.com>
2024-01-18 22:46:27 +1300

    play more with visual in penguins dataset

--- visual_inR.r
+++ visual_inR.r
@@ -23,3 +23,22 @@ ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = bo
 
 ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g), color = "pink")
 
+
+
+
+
+ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g ))
+
+ggplot(data = penguins) + geom_bar(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+ggplot(data = penguins) + geom_line(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+ggplot(data = penguins) + geom_smooth(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+ggplot(data = penguins) + geom_smooth(mapping = aes(x = flipper_length_mm, y = body_mass_g)) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g ))
+
+ggplot(data = penguins) + geom_smooth(mapping = aes(x = flipper_length_mm, y = body_mass_g, linetype = species))
+
+ggplot(data = penguins) + geom_jitter(mapping = aes(x = flipper_length_mm, y = body_mass_g ))
+
+
