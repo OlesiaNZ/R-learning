78fa6e6ddf9bc547b01e0b9f8cf85efa58380732
Olesia Dolzhenkova <olesinionz@gmail.com>
2024-01-19 18:25:02 +1300

    add var to pevent repet the same part of the code describing the basic plot creation

--- lab.r
+++ lab.r
@@ -20,3 +20,12 @@ ggplot(data = penguins) +
   annotate("text", x = 220, y = 3400, label = "The Gentoos are the largest", color = "blue",
            fontface = "bold", size = 4.5, angle = 25)
            
+#Use variable
+peng_plot <- ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)) + 
+  labs(title = "Palmer Penguins: Body Mass vs Flipper Length", 
+       subtitle = "Sample of Three Penguin Species",
+       caption = "Data collected by Dr. Kristen Gorman")
+
+peng_plot + annotate("text", x = 220, y = 3400, label = "The Gentoos are the largest", color = "grey",
+                       fontface = "bold", size = 4.5, angle = 32)
